e65c5366f2256adf6ab53089eae656852b64a6f0
ahong75 <90875315+ahong75@users.noreply.github.com>
2022-05-18 16:51:37 -0400

    Added getting input and output to fasta files
    I originally decided to use command line arguments, but since I'm using VS code I decided against it at the end

--- errorsim/error.cpp
+++ errorsim/error.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
 static char bases[4] = {'A', 'T', 'G', 'C'};
 static double err_prob[3] = {0.02, 0.02, 0.02}; // insertions / deletions / substitutions. Default probability is 0.02
+static double era_error = 0.001;
 
 // Maybe include static variables that represent trackers for what errors have occured thus far
 static int insertions = 0;
 static int deletions = 0;
 static int substitution = 0;
 
-// Generates or does not generate an error based on the probabilities in err_prob
-int create_error() {
+// Generates or does not generate an ids error based on the probabilities in err_prob
+int create_ids_error() {
     double r = double(rand()) / RAND_MAX;
     double sum = 0;
     for (int i = 0; i < 3; i++) {
@@ -24,13 +26,25 @@ int create_error() {
     return 3;
 }
 
+// Generates or does not generate an erasure error for a line in the given fasta file based on the probability of era_error
+bool create_era_error() {
+    double r = double(rand()) / RAND_MAX;
+    if (r < era_error) {
+        return true;
+    }
+    return false;
+}
+
 // Takes in an input DNA string and returns a DNA string with possible insertion / deletion / substitution errors
-string modify(string seq) {
+string ids(string seq) {
     string nseq; // uses a different string as output, so it is not an in-place algorithm
     int len = seq.length();
     for (int i = 0; i < len; i++) {
-        int err = create_error(); // 0 -> insertion, 1 -> deletion, 2 -> substitution, 3 -> no error
+        int err = create_ids_error(); // 0 -> insertion, 1 -> deletion, 2 -> substitution, 3 -> no error
         int nt = rand() % 4; // 0 -> A, 1 -> C, 2 -> T, 3 -> G
+        if (bases[nt] == seq[i] && err == 2) {
+            nt = (nt + (1 + rand() % 2)) % 3; // ensures that for substitutions, bases are not substituted with the same base;
+        }
         if (err == 0 || err == 2) {
             nseq += bases[nt]; // for insertions or substitutions, a random base is added to the new sequence
         }
@@ -52,6 +66,11 @@ string random_string() {
  
 // Can pass in 0 or 3 parameters as doubles from 0 to 1 for error probabilities (other than the program name)
 int main() {
+    cout << "Enter in the name of the input fasta file. Ex. for \"dog.fasta\" enter in \"dog\"" << endl;
+    string file;
+    if (cin.get() != '\n') {
+        cin >> file;
+    }
     cout << "Enter in 3 doubles from 0 to 1 for IDS error probabilites, or press enter to use default error values of 0.02: ";
     if (cin.get() != '\n') {
         for (int i = 0; i < 2; i++) {
@@ -59,13 +78,28 @@ int main() {
         }
     }
     cout << endl;
-    for (int i = 0; i < 100; i++) {
-        string s = random_string();
-        cout << "O: " << endl;
-        cout << s << endl;
-        string modified = modify(s);
-        cout << "E: " << endl;
-        cout << modified << endl;
-        cout << endl;
+    cout << "Enter in 1 double from 0 to 1 for erasure error probability, or press enter to use a default value of 0.001";
+    if (cin.get() != '\n') {
+        cin >> era_error;
     }
+    cout << endl;
+
+    ifstream input(argv[1]); // input stream. default parameter is ios::in
+    ofstream output("output.fasta"); // output stream. default parameter is ios::out
+    string line;
+    while (getline(input, line)) {
+        if (!line.empty()) {
+            if (line[0] == '>') { // if it's a label, just write that same label into the output fasta file
+                output << line << endl;
+            }
+            else {
+                if (create_era_error()) {
+                    continue;
+                }
+                output << ids(line) << endl; // if the line is not erased, add possible ids errors
+            }
+        }
+    }
+    input.close(); output.close();
+    cout << "A modified fasta file has been created." << endl;
 }
