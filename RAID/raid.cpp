0335d8416eb0a36493779622c643f5a5a810e0cd
Anthony Hong <ahong75@gatech.edu>
2022-05-24 09:55:43 -0400

    Added test code

--- RAID/raid.cpp
+++ RAID/raid.cpp
@@ -1,4 +1,5 @@
 #include <cstdlib>
+#include <ctime>
 #include <iostream>
 #include <vector>
 
@@ -19,21 +20,40 @@ std::vector<unsigned char> encode(std::vector<unsigned char> input) {
 }
 
 // decodes a vector with a single corrupted byte, given the encoded vector and
-// location of the missing byte
-std::vector<unsigned char> decode(std::vector<unsigned char> input,
-                                  std::pair<int, int> missing) {
-  int row = missing.first;
-  int col = missing.second;
+// which byte is missing (0 indexed)
+std::vector<unsigned char> decode(std::vector<unsigned char> input, int num) {
+  int col = num % width;
   // deal with the case where the corrupted byte is a parity byte
-  if (row != height + 1) {
+  if (input.size() - num >= width) {
     unsigned char pbyte = input[width * height + col];
     for (int i = 0; i < height; i++) {
-      if (i != row) {
+      if (width * i + col != num) {
         pbyte ^= input[width * i + col];
       }
     }
-    input[width * row + col] = pbyte;
+    input[num] = pbyte;
   }
   return std::vector<unsigned char>(input.begin(), input.end() - width);
 }
-int main() { return 0; }
+
+int main() {
+  // how to test
+  // make random vector size 120 and pass it into encode
+  // choose random number from 1 to 130 (10 xor bytes)
+  // find that bytes' coordinates and then set it to 0 (erasure)
+  // pass it into decode, and see if original vector matches decoded vector
+  for (int i = 0; i < 100; i++) {
+    srand(time(NULL));
+    std::vector<unsigned char> orig(width * height);
+    for (int i = 0; i < width * height; i++) {
+      orig[i] = rand() % 100;
+    }
+    std::vector<unsigned char> encoded = encode(orig);
+    int remove = rand() % (width * height) + 1;
+    encoded[remove] = 0;
+    std::vector<unsigned char> decoded = decode(encoded, remove);
+    if (orig == decoded) {
+      std::cout << "Passed" << std::endl;
+    }
+  }
+}
