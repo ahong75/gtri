aedef80d55998a0eb2368edca3172eb1e0489772
Hong <ahong80@ctisl.gtri.org>
2022-06-06 14:27:57 -0400

    speedtest

--- galois/speedtest.cpp
+++ galois/speedtest.cpp
@@ -3,24 +3,24 @@
 #include <ctime>
 #include <fstream>
 #include <iostream>
+#include <iomanip>
 #include <vector>
 
-const long long N = 1e4;
+const long long N = 3e9;
 using namespace std;
 int main() {
   Galois::G256 field;
   Galois::Elem a(&field, 10);
   Galois::Elem b(&field, 20);
-  // warm-up run
-  // for (long long i = 0; i < N; i++) {
-  //   Galois::Elem c = a * b;
-  // }
+  // warm up run
+  for (long long i = 0; i < N; i++) {
+    Galois::Elem c = a * b;
+  }
   clock_t start = clock();
 
   for (long long i = 0; i < N; i++) {
     Galois::Elem c = a * b;
   }
 
-  float duration = float(clock() - start) / CLOCKS_PER_SEC;
-  cout << 1e9 * duration / N << endl;
+  cout << fixed << setprecision(10) << double(clock() - start) / CLOCKS_PER_SEC << endl;
 }
