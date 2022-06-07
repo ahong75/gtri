0118309116fc4c00d954bfacb0539561a5ec4679
Anthony Hong <ahong75@gatech.edu>
2022-06-07 19:21:12 -0400

    Added crc

---
+++ crc/GF256/crc.cpp
@@ -0,0 +1,30 @@
+#include "G256.hpp"
+#include <vector>
+typedef unsigned char u8;
+
+class crc {
+  // instance vars:
+  // polynomial
+  // data length in bytes
+  // functions:
+  // encode (takes in a byte vector and returns a byte vector with an appended
+  // crc) decode (takes in a byte vector and returns a bool, indicating whether
+  // an error is detected)
+  std::vector<u8> poly;
+  int length;
+  crc(std::vector<u8> input, int len) {
+    // Might want to change this to a simple constant later
+    poly = input;
+    length = len;
+  }
+
+  // Encode function that takes in a byte vector and calculates and appends the
+  // CRC. Might want to change this to a static function later on when we're
+  // just going to be using a single polynomial anyways.
+  void encode(std::vector<u8> &input, Galois::G256 &field) {}
+
+  // Decode function that takes in an encoded byte vector and returns whether or
+  // not an error has been detected according to the CRC
+  bool decode(std::vector<u8> &input, Galois::G256 &field){
+
+  };
