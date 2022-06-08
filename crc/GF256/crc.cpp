b35f44787780a778ddf679ef090ccc8d9708de6f
Anthony Hong <ahong75@gatech.edu>
2022-06-08 12:56:48 -0400

    seperated src and header

--- crc/GF256/crc.cpp
+++ crc/GF256/crc.cpp
@@ -1,30 +1,114 @@
+#include "crc.hpp"
 #include "G256.hpp"
+#include <stdexcept>
 #include <vector>
 typedef unsigned char u8;
 
-class crc {
-  // instance vars:
-  // polynomial
-  // data length in bytes
-  // functions:
-  // encode (takes in a byte vector and returns a byte vector with an appended
-  // crc) decode (takes in a byte vector and returns a bool, indicating whether
-  // an error is detected)
-  std::vector<u8> poly;
-  int length;
-  crc(std::vector<u8> input, int len) {
-    // Might want to change this to a simple constant later
-    poly = input;
-    length = len;
+crc::crc(std::vector<u8> input, int p, int d, Galois::G256 &field) {
+  // Might want to change this to a simple constant later
+  // The size of the poly vector should be 5 bytes because the crc will be 4
+  // bytes
+  for (int i = 0; i < input.size(); i++) {
+    poly.push_back(Galois::Elem(&field, input[i]));
   }
+  data_length = d;
+  poly_length = p;
+}
 
-  // Encode function that takes in a byte vector and calculates and appends the
-  // CRC. Might want to change this to a static function later on when we're
-  // just going to be using a single polynomial anyways.
-  void encode(std::vector<u8> &input, Galois::G256 &field) {}
+std::vector<u8> crc::divide(std::vector<Galois::Elem> &rem,
+                            Galois::G256 &field) {
+  // degree of the highest order term in the poly
+  int pdegree = 0;
+  for (int i = 0; i < poly.size(); i++) {
+    if (poly[i].val != 0) {
+      pdegree = poly.size() - i - 1;
+      break;
+    }
+  }
+  // Boolean that tracks whether or not it is possible to divide the
+  // remaining number by the dividend
+  bool divide = true;
+  while (divide) {
+    // Quotient term initially defaulted to 0
+    Galois::Elem quotient(&field, 0);
+    // Degree of current term of the quotient
+    int qdegree = 0;
+    // We assume that each iteration is the last divison we can perform on the
+    // remainder unless proven otherwise
+    divide = false;
+    // Iterating through the current remainder
+    for (int j = 0; j < rem.size(); j++) {
+      // If the highest possible x degree term has not been found yet, check
+      // if the current term is non zero and that its degree is greater than
+      // or equal to the polynomial degree
+      int ddegree = rem.size() - j - 1;
+      if (rem[j].val != 0 && ddegree >= pdegree) {
+        divide = true;
+        // Dividing the highest order term in the remainder by the highest
+        // order term in the generator polynomial in GF(256)
+        quotient = rem[j] / poly[pdegree];
+        qdegree = ddegree - pdegree;
+        // With this operation, the highest term in the remainder will be
+        // canceled out.
+        rem[j].val = 0;
+        for (int i = pdegree + 1; i < poly.size(); i++) {
+          if (poly[i].val != 0) {
+            // Degree of product between quotient term and current generator
+            // polynomial term
+            int curdegree = (poly.size() - i - 1) * qdegree;
+            // We xor, because that is addition/subtraction in GF(256); Think
+            // of normal polynomial long division
+            rem[rem.size() - curdegree - 1].val ^= poly[i].val;
+          }
+        }
+      }
+    }
+  }
+  std::vector<u8> res(poly_length - 1);
+  for (int i = 0; i < poly_length - 1; i++) {
+    res[i] = rem[rem.size() - poly_length + 1 + i].val;
+  }
+  return res;
+}
 
-  // Decode function that takes in an encoded byte vector and returns whether or
-  // not an error has been detected according to the CRC
-  bool decode(std::vector<u8> &input, Galois::G256 &field){
-
-  };
+void crc::encode(std::vector<u8> &input, Galois::G256 &field) {
+  // Check to see if the input size matches the required length
+  if (data_length != input.size()) {
+    throw std::invalid_argument(
+        "The length of the vector is the wrong size for crc encoding.");
+  }
+  // Vector representing the remainder of the polynomial divide, which is the
+  // crc itself
+  // Wrapping once at the begin and unwrapping at the end is better than
+  // constantly wrapping and unwrapping during the operations.
+  std::vector<Galois::Elem> rem;
+  for (int i = 0; i < data_length; i++) {
+    rem.push_back(Galois::Elem(&field, input[i]));
+  }
+  // Pushing back the zeroed crc
+  for (int i = 0; i < poly_length - 1; i++) {
+    rem.push_back(Galois::Elem(&field, 0));
+  }
+  std::vector<u8> res = divide(rem, field);
+  for (u8 n : res) {
+    input.push_back(n);
+  }
+}
+bool crc::decode(std::vector<u8> &input, Galois::G256 &field) {
+  // Encoded vector should have data bytes + poly_length - 1 crc bytes
+  if (data_length + poly_length - 1 != input.size()) {
+    throw std::invalid_argument(
+        "The length of the vector is the wrong size for crc encoding.");
+  }
+  std::vector<Galois::Elem> rem;
+  for (int i = 0; i < input.size(); i++) {
+    rem.push_back(Galois::Elem(&field, input[i]));
+  }
+  std::vector<u8> res = divide(rem, field);
+  for (u8 n : res) {
+    if (n != 0) {
+      return false;
+    }
+  }
+  return true;
+}
---
+++ crc/GF256/test.cpp
@@ -0,0 +1,10 @@
+#include "G256.hpp"
+#include "crc.hpp"
+
+int main() {
+  // how to test
+  // error detection -> test if it detects errors
+  // make a randomized 20 byte array
+  // make 1-2 errors, see if its always detected, which it should be nearly 100%
+  // of the time
+}
